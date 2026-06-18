d1a55e4c070f6ffe15c82ef6008cc3c93cad6a55
Rajesh Kayal <rajesh-kayal-dev@users.noreply.github.com>
2026-06-18 13:33:44 +0530

    refactor: update index.js response messages and format code style

--- level5/index.js
+++ level5/index.js
@@ -6,13 +6,13 @@ const port=process.env.PORT || 5000
 
 const app=express()
 
-app.get("/health",(req,res)=>{
-   return res.status(200).json({message:"all is good👍"})
+app.get("/health", (req, res) => {
+    return res.status(200).json({ message: "all good" })
 })
-app.get("/",(req,res)=>{
-   return res.status(200).json({message:"hello ayush v4👍"})
+app.get("/", (req, res) => {
+    return res.status(200).json({ message: "hello Rajesh v5" })
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server started ${port}`)
 })
